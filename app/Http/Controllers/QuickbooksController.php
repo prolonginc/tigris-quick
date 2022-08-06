a75858a454d78d89afea73dff669b71d6f7a473b
Sari Yono <sariyono@prolongmarketing.com>
2022-08-06 17:02:44 -0400

    manual dispatch

--- app/Http/Controllers/QuickbooksController.php
+++ app/Http/Controllers/QuickbooksController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\ParseQuickbooksQueue;
 use Illuminate\Http\Request;
 
 class QuickbooksController extends Controller
 {
     public function index()
     {
+        $delay = 0;
+        for ($i=1; $i < 4000; $i++) {
+            ParseQuickbooksQueue::dispatch($i)->delay(now()->addMinutes($delay));
+            if($i %50 == 0) {
+                $delay++;
+            }
+        }
+
         $quickbooks = app('Spinen\QuickBooks\Client');
         $items = [];
         for ($i=1; $i < 9; $i++) {
